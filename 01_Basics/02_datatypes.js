d8cd104808593f2273cf88bb5fd8327bb2ec21a2
Avik Bhattacharya <avikbhattacharyaofficial@gmail.com>
2026-03-06 17:25:55 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); 
+
+// code readability should be high
+
+console.log("Avik")
+
+
+let name ="Avik"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint => when number is very large
+// string => use in ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not defined
+// symbol
+
+//object
+
+console.log(typeof "Avik");
+console.log(typeof null); // output: object
+console.log(typeof undefined);// output: undefined
