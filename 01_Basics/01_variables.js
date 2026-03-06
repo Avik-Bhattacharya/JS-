451f7870b6b47a9cf39b42b5acceee96d9b2380c
Avik Bhattacharya <avikbhattacharyaofficial@gmail.com>
2026-03-06 16:36:49 +0000

    variables in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail="avik@google.com"
+var accountPassword= "12345"
+accountCity ="jaipur"
+let accountState
+
+// accountId = 2
+
+console.log(accountId);
+
+accountEmail = "ab@ab.com"
+accountPassword = "avik@2003"
+accountCity = "kolkata"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
