0e18af3c55436a7fe65533f6a37610dbef122d01
Avik Bhattacharya <avikbhattacharyaofficial@gmail.com>
2026-03-10 15:35:09 +0000

    datatype summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, null,Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue=100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber =  3154535453453354356n
+
+
+// # Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Shaktiman", "naagraj","doga"]; //array
+
+let myObj = {
+    name:"avik",
+    age: 22,
+}// object
+
+const myFunction = function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof heros)
